e5d9f3d30efa796ae205bb09402e4904f194adca
Onur Tuncer <onur.tuncer@melina-aero.com>
2024-03-04 15:02:49 +0300

    cleaned up main.cpp

--- src/ParameterEditor/main.cpp
+++ src/ParameterEditor/main.cpp
@@ -13,7 +13,46 @@
 #include "ParameterManager.h"
 #include "QWrappedParameter.h"
 
+void setupParameters(Controller::ParameterManager& pm1) {
 
+  using namespace Controller;
+
+  pm1.AddParameter("machine.number_of_channels", Parameter<uint8_t>(1));
+  pm1.AddParameter("channels.01.type", Parameter<int>(0)); // 0: milling, 1: turning
+  pm1.AddParameter("channels.01.turning_axis", Parameter<char>('C'));
+  pm1.AddParameter("channels.01.number_of_axes", Parameter<int>(3));
+  pm1.AddParameter("channels.01.kinematics_type", Parameter<int>(0));
+  pm1.AddParameter("channels.01.trajectory.jerklimit", Parameter<double>(50000));
+  pm1.AddParameter("channels.01.trajectory.contouring_enabled", Parameter<bool>(false));
+  pm1.AddParameter("channels.01.trajectory.microspline_enabled", Parameter<bool>(true));
+  pm1.AddParameter("channels.01.trajectory.cornering_tolerance", Parameter<double>(0.4));
+  pm1.AddParameter("channels.01.trajectory.sample_time", Parameter<double>(0.001));
+  pm1.AddParameter("channels.01.trajectory.acceleration_limit", Parameter<double>(4000));
+  pm1.AddParameter("channels.01.trajectory.deceleration_limit", Parameter<double>(4000));
+  pm1.AddParameter("channels.01.servo_names", Parameter<std::vector<std::string>>({"01", "02", "03"}));
+  pm1.AddParameter("bus.io_names", Parameter<std::vector<std::string>>({"01"}));
+  pm1.AddParameter("offsets.g54", Parameter<std::vector<double>>({0.0, 0.0, 0.0}));
+  pm1.AddParameter("offsets.g55", Parameter<std::vector<double>>({0.0, 0.0, 0.0}));
+  pm1.AddParameter("offsets.g56", Parameter<std::vector<double>>({0.0, 0.0, 0.0}));
+  pm1.AddParameter("offsets.g57", Parameter<std::vector<double>>({0.0, 0.0, 0.0}));
+  pm1.AddParameter("offsets.g58", Parameter<std::vector<double>>({0.0, 0.0, 0.0}));
+  pm1.AddParameter("offsets.g59", Parameter<std::vector<double>>({0.0, 0.0, 0.0}));
+  pm1.AddParameter("offsets.activeOffset", Parameter<std::string>("g54"));
+  pm1.AddParameter("tools.T101.diameter", Parameter<double>(10.0));
+  pm1.AddParameter("tools.T101.lengthOffset", Parameter<double>(0.0));
+  pm1.AddParameter("tools.T102.diameter", Parameter<double>(10.0));
+  pm1.AddParameter("tools.T102.lengthOffset", Parameter<double>(0.0));
+  pm1.AddParameter("tools.T103.diameter", Parameter<double>(10.0));
+  pm1.AddParameter("tools.T103.lengthOffset", Parameter<double>(0.0));
+  pm1.AddParameter("tools.T104.diameter", Parameter<double>(10.0));
+  pm1.AddParameter("tools.T104.lengthOffset", Parameter<double>(0.0));
+  pm1.AddParameter("tools.T105.diameter", Parameter<double>(10.0));
+  pm1.AddParameter("tools.T105.lengthOffset", Parameter<double>(0.0));
+  pm1.AddParameter("tools.T106.diameter", Parameter<double>(10.0));
+  pm1.AddParameter("tools.T106.lengthOffset", Parameter<double>(0.0));
+  pm1.AddParameter("tools.T107.diameter", Parameter<double>(10.0));
+  pm1.AddParameter("tools.T107.lengthOffset", Parameter<double>(0.0));
+}
 
 void loadParameter(const QString& paramName, const QVariant& paramValue, TreeModel* model) {
     
@@ -40,26 +79,10 @@ void loadParameter(const QString& paramName, const QVariant& paramValue, TreeMod
         }
     }
 
-    // Check if the final parameter node already exists
-    bool finalNodeExists = false;
-    for (int i = 0; i < parent->childCount(); ++i) {
-        if (parent->getChild(i)->data().value<ParameterEntry>().key() == hierarchy.last()) {
-            finalNodeExists = true;
-            break;
-        }
-    }
-
-    // Add the final parameter node if it doesn't exist already
-    if (!finalNodeExists) {
-        ParameterEntry entry(hierarchy.last(), paramValue, 0);
-        TreeItem* parameterNode = new TreeItem(QVariant::fromValue(entry));
-        model->addItem(parent, parameterNode);
-    }
-
-    // // Add the final parameter node
-    // ParameterEntry entry(hierarchy.last(), paramValue, 0);
-    // TreeItem* parameterNode = new TreeItem(QVariant::fromValue(entry));
-    // model->addItem(parent, parameterNode);
+    // Add the final parameter node
+    ParameterEntry entry(hierarchy.last(), paramValue, 0);
+    TreeItem* parameterNode = new TreeItem(QVariant::fromValue(entry));
+    model->addItem(parent, parameterNode);
 }
 
 void populateModelFromParameterManager(Controller::ParameterManager& paramManager, TreeModel* parameterModel) {
@@ -72,87 +95,27 @@ void populateModelFromParameterManager(Controller::ParameterManager& paramManage
     }
 }
 
-
-/* void populateModel(TreeModel& model)
-{
-    QFile jsonFile{":/sample.json"};
-    if (!jsonFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
-        qCritical() << "error: json file cannot be open";
-        return;
-    }
-
-    QJsonParseError error;
-    auto doc = QJsonDocument::fromJson(jsonFile.readAll(), &error);
-    qDebug() << "loading json file:" << error.errorString();
-
-    auto root = doc.isArray() ? QJsonValue(doc.array()) : QJsonValue(doc.object());
-    loadValue(root, model.rootItem(), &model);
-}
- */
-
 int main(int argc, char* argv[]){
 
- using namespace Controller;
+  using namespace Controller;
 
   ParameterManager pm1;
+  setupParameters(pm1);
+  
+  QGuiApplication app(argc, argv);
+  QQmlApplicationEngine engine;
 
-  pm1.AddParameter("machine.number_of_channels", Parameter<uint8_t>(1));
-  pm1.AddParameter("channels.01.type", Parameter<int>(0)); // 0: milling, 1: turning
-  pm1.AddParameter("channels.01.turning_axis", Parameter<char>('C'));
-  pm1.AddParameter("channels.01.number_of_axes",       Parameter<int>(3));
-  pm1.AddParameter("channels.01.kinematics_type",      Parameter<int>(0)); 
-  pm1.AddParameter("channels.01.trajectory.jerklimit" , Parameter<double>(50000));
-  pm1.AddParameter("channels.01.trajectory.contouring_enabled", Parameter<bool>(false));
-  pm1.AddParameter("channels.01.trajectory.microspline_enabled", Parameter<bool>(true));
-  pm1.AddParameter("channels.01.trajectory.cornering_tolerance",  Parameter<double>(0.4));
-  pm1.AddParameter("channels.01.trajectory.sample_time",  Parameter<double>(0.001));
-  pm1.AddParameter("channels.01.trajectory.acceleration_limit",  Parameter<double>(4000));
-  pm1.AddParameter("channels.01.trajectory.deceleration_limit",  Parameter<double>(4000));
-  pm1.AddParameter("channels.01.servo_names", Parameter<std::vector<std::string>>({"01","02","03"}));
-  pm1.AddParameter("bus.io_names", Parameter<std::vector<std::string>>({"01"}));
-  pm1.AddParameter("offsets.g54"              , Parameter<std::vector<double>>({0.0, 0.0, 0.0}));
-  pm1.AddParameter("offsets.g55"              , Parameter<std::vector<double>>({0.0, 0.0, 0.0}));
-  pm1.AddParameter("offsets.g56"              , Parameter<std::vector<double>>({0.0, 0.0, 0.0}));
-  pm1.AddParameter("offsets.g57"              , Parameter<std::vector<double>>({0.0, 0.0, 0.0}));
-  pm1.AddParameter("offsets.g58"              , Parameter<std::vector<double>>({0.0, 0.0, 0.0}));
-  pm1.AddParameter("offsets.g59"              , Parameter<std::vector<double>>({0.0, 0.0, 0.0}));
-  pm1.AddParameter("offsets.activeOffset"     , Parameter<std::string>("g54")); 
-  pm1.AddParameter("tools.T101.diameter"      , Parameter<double>(10.0));
-  pm1.AddParameter("tools.T101.lengthOffset"  , Parameter<double>(0.0));
-  pm1.AddParameter("tools.T102.diameter"      , Parameter<double>(10.0));
-  pm1.AddParameter("tools.T102.lengthOffset"  , Parameter<double>(0.0));
-  pm1.AddParameter("tools.T103.diameter"      , Parameter<double>(10.0));
-  pm1.AddParameter("tools.T103.lengthOffset"  , Parameter<double>(0.0));
-  pm1.AddParameter("tools.T104.diameter"      , Parameter<double>(10.0));
-  pm1.AddParameter("tools.T104.lengthOffset"  , Parameter<double>(0.0));
-  pm1.AddParameter("tools.T105.diameter"      , Parameter<double>(10.0));
-  pm1.AddParameter("tools.T105.lengthOffset"  , Parameter<double>(0.0));
-  pm1.AddParameter("tools.T106.diameter"      , Parameter<double>(10.0));
-  pm1.AddParameter("tools.T106.lengthOffset"  , Parameter<double>(0.0));
-  pm1.AddParameter("tools.T107.diameter"      , Parameter<double>(10.0));
-  pm1.AddParameter("tools.T107.lengthOffset"  , Parameter<double>(0.0));
-
-
-QGuiApplication app(argc, argv);
-QQmlApplicationEngine engine;
-
-QObject::connect(&engine, &QQmlApplicationEngine::warnings, [](const QList<QQmlError> &warnings) {
+  QObject::connect(&engine, &QQmlApplicationEngine::warnings, [](const QList<QQmlError> &warnings) {
     for (const auto &warning : warnings) {
         qDebug() << "QML Warning:" << warning.toString();
     }
-});
+  });
   
 qmlRegisterType<TreeModel>("QMLTreeView", 1, 0, "TreeModel");
 
 auto parameterModel = new TreeModel(&engine); // Assuming you have a parent object for your tree model
     
 populateModelFromParameterManager(pm1, parameterModel);
-
-// loadParameter(QString("machine.number_of_channels"), QWrappedParameter(Parameter<uint8_t>(1)).toQVariant(), parameterModel);
-// loadParameter(QString("channels.01.type"), QWrappedParameter(Parameter<int>(100)).toQVariant(), parameterModel);
-// loadParameter(QString("channels.01.number_of_axes"), QWrappedParameter(Parameter<int>(3)).toQVariant(), parameterModel);
-// loadParameter(QString("channels.01.trajectory.contouring_enabled"), QWrappedParameter(Parameter<bool>(true)).toQVariant(), parameterModel);
-// loadParameter(QString("channels.01.servo_names"), QWrappedParameter(Parameter<std::vector<std::string>>({"01","02","03"})).toQVariant(), parameterModel);
 
 engine.rootContext()->setContextProperty("parameterModel", parameterModel);
 
